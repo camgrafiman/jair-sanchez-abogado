c8e8143ffaa4fd30d5c79a9d088b9a0009938b5e
Alejandro Gallego <camgrafiman@gmail.com>
2021-02-11 11:39:16 +0100

    cambiadas variables de entorno 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         MONGO_DB_URI: "mongodb+srv://administradorJS:gGAiT6eWFin6KHrK@clusterprincipal.xdklf.mongodb.net/jair-sanchez-db?retryWrites=true&w=majority",
-        GRAPHQL_URI: "http://localhost:3000/api/graphql",
+        GRAPHQL_URI: "https://jair-sanchez-abogado.vercel.app/api/graphql",
         SALUDO:"Hola mundo cliente"
     }
 }
