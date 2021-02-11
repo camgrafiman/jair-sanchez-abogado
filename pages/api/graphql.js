c1588002785d47881fe173fa60ed222819874de7
Alejandro Gallego <camgrafiman@gmail.com>
2021-02-11 16:25:22 +0100

    cambios en api/graphql

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,16 +1,123 @@
-import { ApolloServer, makeExecutableSchema } from 'apollo-server-micro';
-import { typeDefs } from './schemas/index';
-import { resolvers } from './resolvers/index';
+import { ApolloServer, gql } from 'apollo-server-micro';
 /* Cliente de mongo para acceder a la base de datos */
 import { MongoClient } from 'mongodb';
 
 
+const typeDefs = gql`
+    type Usuario {
+        _id: ID!
+        nombre: String
+        apellido: String
+        password: String
+        email: String
+        anoNacimiento: Int
+        web: String
+        tipo: String
+    }
+
+    type Query {
+        getUsuarios: [Usuario]!
+        getUsuariosTipo(tipo: String!) : [Usuario]
+        getUsuario(_id: Int!): Usuario
+    }
+
+`;
+
+const resolvers = { 
+    Query: {
+        getUsuarios: async (_parent, _args, _context, _info) => {
+            try {
+                /* retornar datos de la coleccion_usuarios en la database conectada: */
+                return await _context.db.collection('coleccion_usuarios').findOne().then((data) => {
+                    // Mismo que en la data de la coleccion, ver mongoDB Atlas:
+                    console.log(data)
+                    return data.usuarios
+                })
+            }
+            catch (error) {
+                throw error;
+            }
+        },
+        getUsuariosTipo: async (_parent, args, _context) => {
+            try {
+                // console.log("nombre", args.nombre);
+                const {tipo} = args;
+                /* retornar datos de la coleccion_usuarios en la database conectada: */
+                return await _context.db.collection('coleccion_usuarios').findOne().then((data) => {
+                    // Mismo que en la data de la coleccion, ver mongoDB Atlas:
+                    // console.log(data.usuarios);
+                    const usuarioTipo = data.usuarios.filter(usuario => {
+                        return usuario.tipo == tipo;
+                    })
+                    return usuarioTipo
+                })
+                
+                
+            } catch (error) {
+                throw error;
+            }
+        },
+        getUsuario: async (_parent, args, _context) => {
+            try {
+                // console.log("nombre", args.nombre);
+                const {_id} = args;
+                /* retornar datos de la coleccion_usuarios en la database conectada: */
+                return await _context.db.collection('coleccion_usuarios').findOne().then((data) => {
+                    // Mismo que en la data de la coleccion, ver mongoDB Atlas:
+                    // console.log(data.usuarios);
+                    const usuarioSel = data.usuarios.filter(usuario => {
+                        return usuario._id == _id;
+                    })[0]
+                    return usuarioSel
+                })
+                
+                
+            } catch (error) {
+                throw error;
+            }
+        }
+    }
+    // Query: {
+    //     getUsuarios: async () => {
+    //         try {
+    //             const usuarios = await axios.get("https://api.github.com/users");
+    //             return usuarios.data.map(({ id, login, avatar_url, name, company, blog }) => ({
+    //                 id,
+    //                 login,
+    //                 avatar_url,
+    //                 name,
+    //                 company,
+    //                 blog
+    //             }));
+    //         }
+    //         catch (error) {
+    //             throw error;
+    //         }
+    //     },
+    //     getUsuario: async (_, args) => {
+    //         try {
+    //             const usuario = await axios.get(`https://api.github.com/users/${args.nombre}`);
+    //             return {
+    //                 id: usuario.data.id,
+    //                 login: usuario.data.login,
+    //                 avatar_url: usuario.data.avatar_url,
+    //                 name: usuario.data.name,
+    //                 company: usuario.data.company,
+    //                 blog: usuario.data.blog
+    //             };
+    //         } catch (error) {
+    //             throw error;
+    //         }
+    //     }
+    // }
+}
+
 
 /* Se crea el esquema ejecutable pasandole los schemas definidos (typeDefs) y los resolvers*/
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-})
+// const schema = makeExecutableSchema({
+//   typeDefs,
+//   resolvers,
+// })
 
 let db
 
@@ -18,9 +125,9 @@ let db
 
 // Crear el servidor a partir de una instancia de ApolloServer:
 const apolloServer = new ApolloServer({
-    schema,
-    // typeDefs,
-    // resolvers,
+    // schema,
+    typeDefs,
+    resolvers,
     context: async () => {
         if (!db) {
             try {
