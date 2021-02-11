676d2ec2d015c112e06f0819c38dfe27f6b22ec9
Alejandro Gallego <camgrafiman@gmail.com>
2021-02-11 11:44:46 +0100

    agregadas variables dev-production

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        MONGO_DB_URI: "mongodb+srv://administradorJS:gGAiT6eWFin6KHrK@clusterprincipal.xdklf.mongodb.net/jair-sanchez-db?retryWrites=true&w=majority",
-        GRAPHQL_URI: "https://jair-sanchez-abogado.vercel.app/api/graphql",
-        SALUDO:"Hola mundo cliente"
-    }
-}
