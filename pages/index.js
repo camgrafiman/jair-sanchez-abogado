74c93f1dc15ef99edb92693724ec34e7b0bd88af
Alejandro Gallego <camgrafiman@gmail.com>
2021-02-12 19:16:32 +0100

    arreglado fallo en apollo 1

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import ContactFormulario from '../components/ContactFormulario';
 
 
 const Index = (props) => {
-    console.log("datos de getStaticProps: ", props.usuarios);
+    console.log("datos de getStaticProps: ", props);
     return (
         <>
             <ContainerPrincipal
@@ -31,84 +31,84 @@ const Index = (props) => {
 export default Index;
 
 // //getServerSideProps 
-// export async function getStaticProps(context) {
+export async function getStaticProps(context) {
 
-//     /* Traer datos API SPACEX - EXTERNA */
+    /* Traer datos API SPACEX - EXTERNA */
 
-//     // const client = new ApolloClient({
-//     //     uri: 'https://api.spacex.land/graphql',
-//     //     cache: new InMemoryCache()
-//     // })
+    // const client = new ApolloClient({
+    //     uri: 'https://api.spacex.land/graphql',
+    //     cache: new InMemoryCache()
+    // })
 
-//     // const { data } = await client.query({
-//     //     query: gql`
-//     //     query GetLaunches {
-//     //         launchesPast(limit:10) {
-//     //             id
-//     //             mission_name
-//     //             launch_date_local
-//     //             launch_site {
-//     //                 site_name_long
-//     //             }
-//     //             links {
-//     //                 article_link
-//     //                 video_link
-//     //                 mission_patch
-//     //             }
-//     //             rocket {
-//     //                 rocket_name
-//     //             }
-//     //         }
-//     //     }
+    // const { data } = await client.query({
+    //     query: gql`
+    //     query GetLaunches {
+    //         launchesPast(limit:10) {
+    //             id
+    //             mission_name
+    //             launch_date_local
+    //             launch_site {
+    //                 site_name_long
+    //             }
+    //             links {
+    //                 article_link
+    //                 video_link
+    //                 mission_patch
+    //             }
+    //             rocket {
+    //                 rocket_name
+    //             }
+    //         }
+    //     }
         
-//     //     `
-//     // })
+    //     `
+    // })
 
-// /* USAR API GRAPHQL  */
-//     console.log('URL DE GRAPHQL: ', process.env.GRAPHQL_URI)
-//     console.log(process.env.SALUDO)
+    /* USAR API GRAPHQL  */
+    // console.log('URL DE GRAPHQL: ', process.env.GRAPHQL_URI)
+    // console.log(process.env.SALUDO)
 
-//     const client = new ApolloClient({
-//         ssrMode: typeof window === "undefined",
-//         link: new HttpLink({
-//             uri: process.env.GRAPHQL_URI,
-//             // credentials: 'same-origin'
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Access-Control-Allow-Credentials': true,
-//             }
-//         }),
-//         cache: new InMemoryCache(),
+    const client = new ApolloClient({
+        ssrMode: typeof window === "undefined",
+        link: new HttpLink({
+            uri: process.env.GRAPHQL_URI,
+            // credentials: 'same-origin'
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            }
+        }),
+        cache: new InMemoryCache(),
         
-//     })
+    })
 
    
-//         const {data} = await client.query({
-//                 query: gql`
-//                 query users {
-//                     getUsuarios {
-//                         _id
-//                         nombre
-//                         apellido
-//                         email
-//                     }
-//                 }
+    const {data} = await client.query({
+        query: gql`
+            query users {
+                getUsuarios {
+                    _id
+                    nombre
+                    apellido
+                    email
+                }
+            }
                 
-//                 `
-//             })
+            `
+         })
 
-//             console.log('data ', data.getUsuarios);
+    //console.log('data ', data);
     
     
 
     
 
-//     return {
-//         props: {
-//             usuarios: data.getUsuarios
-//         }, // Será pasado al componente pagina como props.
-//     }
-// }
+    return {
+        props: {
+            usuarios: data
+        }, // Será pasado al componente pagina como props.
+    }
+}
 
 
