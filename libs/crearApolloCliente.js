1df064212c91038154ff154e04865438e9196853
Alejandro Gallego <camgrafiman@gmail.com>
2021-02-17 18:11:46 +0100

    solucionada la mutation y el envio de formulario

--- libs/crearApolloCliente.js
+++ libs/crearApolloCliente.js
@@ -5,12 +5,12 @@ export default function crearApolloCliente(estadoInicial, ctx) {
     // The `ctx` (NextPageContext) will only be present on the server.
     // use it to extract auth headers (ctx.req) or similar.
     console.log("crearApolloCliente ejecutado");
-    console.log("graphql uri: ", process.env.GRAPHQL_URI);
+    console.log("graphql uri: ", process.env.NEXT_PUBLIC_GRAPHQL_URL);
     const client = new ApolloClient({
         // ssrMode: Boolean(ctx),
         link: new HttpLink({
-            // uri: process.env.GRAPHQL_URI, // Server URL (must be absolute)
-            uri: "https://jair-sanchez-abogado.vercel.app/api/graphql",
+            uri: process.env.NEXT_PUBLIC_GRAPHQL_URL, // Server URL (must be absolute)
+            //uri: "https://jair-sanchez-abogado.vercel.app/api/graphql",
             // credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
             headers: {
                 'Content-Type': 'application/json',
